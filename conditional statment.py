05fad317f087596cbb5c2ba5e7ab7ab198c07865
Ahmad Ali <spyali463@gmail.com>
2024-12-06 21:32:05 +0500

    conditional statement

---
+++ conditional statment.py
@@ -0,0 +1,58 @@
+# types  of string three 
+
+# var1 = "university"
+# var2 = 'fsd'
+# var3 = """Artificical"""
+# print(var1,var2, var3)
+# check length
+# print(len(var1))
+
+# indexing check 
+
+
+
+# var1 = "punjab college"
+# ch = var1[2]
+# print(ch)
+
+
+# how to use slicing
+
+# var1 = "punjab college"
+# ch = var1[2 : 10]
+# print(ch)
+
+
+
+# negative index
+
+# var1 = "punjab college"
+# ch = var1[-10 : -1]
+# ch = var1[-14 :]
+# ch = var1[ :-1]
+# print(ch)
+
+
+# string ends check
+
+str = "I am studying python"
+
+print(str.endswith("on"))
+
+# how to first word capitalize 
+
+str = "i am studying python"
+
+print(str.capitalize())
+
+# hpw to replace word
+
+
+str = "I am studying python"
+
+print(str.replace("y","t"))
+
+
+str = "I am studying python"
+
+print(str.find("t"))
---
+++ practice.py
@@ -0,0 +1,23 @@
+# int = input("enter A number:")
+# ch = len(int)
+# print(ch)
+
+
+
+
+
+
+
+
+# str = "one $ is equal to 300 $"
+# str = str.count("$")
+# print(str)
+
+
+num = int(input("Enter your number"))
+
+# rem = num % 2
+if(num%2==0):
+    print("number is even")
+else:
+    print("the number is odd")
